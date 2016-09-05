a1809df990e93eefaa702800b997409b38d89302
Skole <Skole@RASMUSRS-PC>
2016-09-05 09:11:16 +0200

    Add project files.

---
+++ 00_Hello/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _00_Hello
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestHello()
+        {
+            Assert.AreEqual("Hello", Functions.Hello());
+        }
+        [TestMethod]
+        public void TestHelloFred()
+        {
+            Assert.AreEqual("Hello, Fred", Functions.Hello("Fred"));
+        }
+    }
+
+    internal class Functions
+    {
+        public static String Hello(string name = "")
+        {
+            if (name == "")
+            {
+                return "Hello";
+            } else
+            {
+                return "Hello, " + name;
+            }
+            
+        }
+    }
+}
